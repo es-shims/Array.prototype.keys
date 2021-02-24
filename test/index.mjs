cc74877edc242817294dd17b67b6fa6933bb8a28
Jordan Harband <ljharb@gmail.com>
2021-02-23 18:39:19 -0800

    [Dev Deps] update `eslint`, `@ljharb/eslint-config`, `aud`, `functions-have-names`, `has-strict-mode`, `tape`

--- test/index.mjs
+++ test/index.mjs
@@ -19,7 +19,7 @@ test('named exports', async (t) => {
 	t.deepEqual(
 		Object.keys(keysModule).sort(),
 		['default', 'shim', 'getPolyfill', 'implementation'].sort(),
-		'has expected named exports'
+		'has expected named exports',
 	);
 
 	const { shim, getPolyfill, implementation } = keysModule;
