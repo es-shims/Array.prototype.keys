ff4ed72d47f2b9170e0d92969e53f82bd251d83b
Jordan Harband <ljharb@gmail.com>
2022-04-11 18:21:32 -0700

    [New] `shim`/`auto`: add `keys` to `Symbol.unscopables`

--- shim.js
+++ shim.js
@@ -1,14 +1,20 @@
 'use strict';
 
 var define = require('define-properties');
+var shimUnscopables = require('es-shim-unscopables');
+
 var getPolyfill = require('./polyfill');
 
 module.exports = function shimArrayKeys() {
 	var polyfill = getPolyfill();
+
 	define(
 		Array.prototype,
 		{ keys: polyfill },
 		{ keys: function () { return Array.prototype.keys !== polyfill; } }
 	);
+
+	shimUnscopables('keys');
+
 	return polyfill;
 };
